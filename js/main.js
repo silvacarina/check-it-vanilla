a828b29c104f9353b610bb8185507a7f259db63e
Carina <cass.carinasilva@gmail.com>
2022-03-01 22:20:26 -0300

    Adicionado  JS inicial para adicionar tarefa

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
+const $form = document.getElementById('form-tasks');
 
+const adicionarTarefa = (title) => {
+    const $taskHTML = `<li class="tasks-section__item">
+    <label class="tasks-section__item__label">
+      <input type="checkbox" class="tasks-section__item__checkbox">
+      <span class="tasks-section__item__text">${title}</span>
+    </label>
+    <button class="tasks-section__item__remove">X</button>
+  </li>`
+
+    const $taskList = document.getElementById('list-todo')
+
+    $taskList.insertAdjacentHTML('beforeend', $taskHTML)    
+}
+
+const envioDeFormulario = (event) => {
+    event.preventDefault()
+    const formData = new FormData($form)
+    const title = formData.get('title')
+
+    adicionarTarefa(title)
+}
+
+$form.addEventListener('submit', envioDeFormulario)
